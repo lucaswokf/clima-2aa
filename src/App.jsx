c148ffc0b201a4c7e09c8b0c352ee69673a77a63
lucaswokf <lucasinfu@gmail.com>
2025-10-01 10:56:56 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <div className="app-container">
+      <div className="content-wrapper">
+        <header>
+        <h1>Consulta da Clima </h1>
+        <p>Exemplo de consumo de API com React</p>
+        </header>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default App
